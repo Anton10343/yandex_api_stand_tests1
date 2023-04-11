b55e8132d619dda70a327fa54ba1a92f669ebcf2
Anton <anton0leinikov@yandex.ru>
2023-04-11 21:54:07 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://e8ad4265-928a-4272-bdd6-b94f947fd583.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
